8fe7d31cd398fa077dbb3feda053c9347a3ec219
Сорокин Георгий <georgy_sorokin@relex.ru>
2020-05-05 22:09:28 +0300

    axes rotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React, {useEffect, Suspense} from "react";
 import {useThree, Canvas} from "react-three-fiber";
+import {Vector3, AxesHelper} from "three";
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-import { Box }from "./simple/box/box"
+import {Box} from "./simple/box/box"
 
 const CameraController = () => {
     const {camera, gl} = useThree();
+    console.log(camera);
     useEffect(
         () => {
             const controls = new OrbitControls(camera, gl.domElement);
             controls.minDistance = 1
-            controls.maxDistance = 10
+            controls.maxDistance = 100
             controls.enableKeys = false
+            controls.enablePan = false
+            controls.target.set(50,0,50);
+            controls.update();
             return () => {
                 controls.dispose();
             }
@@ -22,10 +27,16 @@ const CameraController = () => {
 
 function App() {
     return (
-        <div style={{ height: window.innerHeight}}>
-            <Canvas>
+        <div style={{height: window.innerHeight}}>
+            <Canvas
+                camera={{
+                    fov: 100,
+                    position: new Vector3(-50, 50, 50),
+                }}
+            >
                 <ambientLight/>
                 <CameraController/>
+                <primitive object={new AxesHelper(110)}/>
                 <Suspense fallback={null}>
                     <Box/>
                 </Suspense>
--- src/simple/box/box.tsx
+++ src/simple/box/box.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {useLoader} from "react-three-fiber";
-import {TextureLoader} from "three";
+import {TextureLoader, AxesHelper} from "three";
 import image from "./textures/side.png"
 interface Props {
 
@@ -9,9 +9,10 @@ export const Box = () => {
     const texture = useLoader(TextureLoader, image);
     console.log("render");
     return (
-        <mesh position={[0, 0, 0]}>
-            <boxBufferGeometry attach="geometry" args={[1, 1, 1]}/>
-            <meshStandardMaterial attach="material" map={texture}/>
+        <mesh position={[50, 0, 50]} rotation={[0,Math.PI/2,0]}>
+            <boxBufferGeometry attach="geometry" args={[100, 1, 100]}/>
+            <meshStandardMaterial attach="material" color={0x00ff00}/>
+            <primitive object={new AxesHelper(100)}/>
         </mesh>
     );
 }
